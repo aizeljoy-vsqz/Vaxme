d6c57e4933157d996c0b5a9ee7343164d6a32fd9
aizeljoy-vsqz <vasquezaizeljoyp@gmail.com>
2026-03-06 22:54:35 +0800

    Create general inventory page layout

--- resources/js/pages/Inventory/inventory.tsx
+++ resources/js/pages/Inventory/inventory.tsx
@@ -16,7 +16,20 @@ export default function Inventory() {
         <AppLayout breadcrumbs={breadcrumbs}>
             <Head title="Dashboard" />
             <div className="flex h-full flex-1 flex-col gap-4 overflow-x-auto rounded-xl p-4">
+                {/* container for label & button */}
+                <div className='flex justify-between'>
+                    <div className='rounded-xl border'>
+                        <PlaceholderPattern></PlaceholderPattern>
+                    </div>
+                    <div className='rounded-xl border'>
+                        <PlaceholderPattern></PlaceholderPattern>
+                    </div>
+                </div>
 
+                {/* container for table */}
+                <div className='rounded-xl border'>
+                    <PlaceholderPattern></PlaceholderPattern>
+                </div>
             </div>
         </AppLayout>
     );
