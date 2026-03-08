db6066afad61b5e182805cda9900b43edc0220ce
Jzzz <maganajohncarlodelossantos@gmail.com>
2026-03-08 12:41:31 +0800

    Create general vaccine inventory form

--- resources/js/pages/Inventory/general-inventory/inventory.tsx
+++ resources/js/pages/Inventory/general-inventory/inventory.tsx
@@ -1,13 +1,26 @@
 import { Head } from '@inertiajs/react';
 import AppLayout from '@/layouts/app-layout';
 import type { BreadcrumbItem } from '@/types';
-import { PlaceholderPattern } from '@/components/ui/placeholder-pattern';
 import { Plus } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 import { columns, Payment } from './columns';
 import { DataTable } from '../data-table';
 import { Separator } from '@/components/ui/separator';
-
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog"
+import {
+    Field,
+    FieldGroup,
+    FieldLabel,
+} from "@/components/ui/field"
+import { Input } from "@/components/ui/input"
+import { DatePickerInput } from '@/components/date-picker';
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
@@ -39,9 +52,53 @@ export default function Inventory() {
                 {/* container for label & button */}
                 <div className='flex justify-between items-center'>
                     <label className='text-sm font-medium'>Vaccine Inventory</label>
-                    <Button className='bg-blue-500 hover:bg-blue-700'><Plus />Add inventory</Button>
+                    <Dialog>
+                        <DialogTrigger>
+                            <Button className='bg-blue-500 hover:bg-blue-700'><Plus />Add inventory</Button>
+                        </DialogTrigger>
+                        <DialogContent>
+                            <DialogHeader>
+                                <DialogTitle>Vaccine</DialogTitle>
+                                <DialogDescription>
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                </DialogDescription>
+                            </DialogHeader>
+                            <FieldGroup>
+                                <Field>
+                                    <FieldLabel htmlFor="username">Name of Health Faculty</FieldLabel>
+                                    <Input id="username" type="text" placeholder="e.g. Rhu-1" />
+                                </Field>
+                                <div className='grid grid-cols-2 gap-4'>
+                                    <Field>
+                                        <FieldLabel htmlFor="username">Name of Drug</FieldLabel>
+                                        <Input id="username" type="text" placeholder="e.g. Penta" />
+                                    </Field>
+                                    <Field>
+                                        <FieldLabel htmlFor="username">Batch Number</FieldLabel>
+                                        <Input id="username" type="text" placeholder="e.g. 00126" />
+                                    </Field>
+                                </div>
+                                <div className='grid grid-cols-2 gap-4'>
+                                    <Field>
+                                        <FieldLabel htmlFor="username">Stock Received</FieldLabel>
+                                        <Input id="username" type="text" placeholder="e.g. 20" />
+                                    </Field>
+                                    <Field>
+                                        <FieldLabel htmlFor="username">Expiration Date</FieldLabel>
+                                        <DatePickerInput />
+                                    </Field>
+                                </div>
+                                <Field orientation="horizontal" className='flex w-full justify-end'>
+                                    <Button variant="outline" type="button">
+                                        Cancel
+                                    </Button>
+                                    <Button type="submit" className='bg-blue-500 hover:bg-blue-700'>Submit</Button>
+                                </Field>
+                            </FieldGroup>
+                        </DialogContent>
+                    </Dialog>
                 </div>
-                <Separator/>
+                <Separator />
                 {/* container for table */}
                 <div className='px-2'>
                     <DataTable columns={columns} data={data} />
