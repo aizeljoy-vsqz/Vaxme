81eed20fc6c8a25da4a6489b17e868302717b128
Aiz <vasquezaizeljoyp@gmail.com>
2026-03-09 15:36:56 +0800

    test

--- resources/js/pages/dashboard.tsx
+++ resources/js/pages/dashboard.tsx
@@ -30,6 +30,10 @@ export default function Dashboard() {
                 <div className="relative min-h-[100vh] flex-1 overflow-hidden rounded-xl border border-sidebar-border/70 md:min-h-min dark:border-sidebar-border">
                     <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
                 </div>
+                <div className="relative min-h-[100vh] flex-1 overflow-hidden rounded-xl border border-sidebar-border/70 md:min-h-min dark:border-sidebar-border">
+                    <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
+                    <h3>test</h3>
+                </div>
             </div>
         </AppLayout>
     );
